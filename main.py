1351c74174c3aafc0291206e67384789de663998
maujacobo <david.jacobo@utec.edu.pe>
2022-10-14 12:27:27 -0500

    Primer commit Proyecto 1.1 Progra

---
+++ main.py
@@ -0,0 +1,15 @@
+import opciones
+
+print("VENTA DE AUTOS")
+while True:
+    print("1   Registrar Autos \n2   Autos disponibles\n3   Comprar Auto")
+    
+    opcion = int(input("Seleccione una opción: \n"))
+    if opcion == 1:
+        opciones.registro()
+        break
+    if opcion == 2:
+        opciones.disponibles()
+        break
+    if opcion == 3:
+        break
---
+++ opciones.py
@@ -0,0 +1,74 @@
+from tabulate import tabulate
+
+def registro():
+
+    # Petición y validación de datos 
+    marca = input("Marca: ").capitalize()
+    fabricacion = int(input("Año de fabricación: "))
+    color = input("Color: ").lower()
+    precio = int(input("Precio (S/):"))
+    estado = "disponible"
+
+    # Añadir el auto como lista al archivo autos_db.txt
+    datos = [marca, fabricacion, color, precio,estado]
+    texto = []
+    with open("autos_db.txt","r",encoding="UTF-8") as F:
+        texto = F.readlines()
+        for i in range(len(texto)):
+            texto[i]=texto[i].replace("\n", "")
+        F.close()
+    
+    if str(datos) in texto:
+        print("Este auto ya se encuentra registrado \n")
+    else:
+        with open("autos_db.txt", "a", encoding="UTF-8") as F:
+            F.write(str(datos) + "\n")
+            F.close
+        print("AUTO RERGISTRADO")
+
+def disponibles():
+    autos = []
+    tabla=[]
+
+    with open("autos_db.txt", "r", encoding="UTF-8") as F:
+        autos = F.readlines()
+        F.close()
+
+    for i in range(len(autos)):
+        auto_ = autos[i].replace("\n","").replace("[","").replace("]","").replace(" ","").replace("'","").split(",")
+
+        if auto_[4] == "disponible":
+            tabla.append(auto_)
+        else:
+            continue
+    
+    print(tabulate(tabla, headers=["MARCA", "FABRICACIÓN", "COLOR", "PRECIO", "ESTADO"]))
+
+def venta():
+    # Petición y validación de datos 
+    marca = input("Marca: ").capitalize()
+    fabricacion = int(input("Año de fabricación: "))
+    color = input("Color: ").lower()
+    precio = int(input("Precio (S/):"))
+
+    auto_seleccionado = [marca, fabricacion, color, precio, "disponible"]
+    autos=[]
+
+    with open("autos_db.txt", "r", encoding="UTF-8") as F:
+        autos = F.readlines()
+        F.close()
+    
+    for i in range(len(autos)):
+        if autos[i].replace("\n","") == str(auto_seleccionado):
+            autos[i] = autos[i].replace("disponible", "vendido")
+    
+    with open("autos_db.txt", "a", enconding="UTF-8") as F:
+        for i in range(len(autos)):
+            pass
+            
+
+    print(autos)
+    print(str(auto_seleccionado))
+    
+
+venta()
