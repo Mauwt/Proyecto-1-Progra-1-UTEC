bf34c972f6a35fede48dd0c93936655e1f64c772
Mauricio Jacobo <david.jacobo@utec.edu.pe>
2022-11-03 10:56:31 -0600

    posible commit final

--- main.py
+++ main.py
@@ -2,19 +2,71 @@ import opciones
 import time
 import os
 import platform
+import sys
+
+def animacion_de_carga():
+  print("cargando:")
+  
+  #animacion = ["10%", "20%", "30%", "40%", "50%", "60%", "70%", "80%", "90%", "100%"]
+  animation = ["[■□□□□□□□□□]","[■■□□□□□□□□]", "[■■■□□□□□□□]", "[■■■■□□□□□□]", "[■■■■■□□□□□]", "[■■■■■■□□□□]", "[■■■■■■■□□□]", "[■■■■■■■■□□]", "[■■■■■■■■■□]", "[■■■■■■■■■■]"]
+  
+  for i in range(len(animation)):
+      time.sleep(0.2)
+      sys.stdout.write("\r" + animation[i % len(animation)])
+      sys.stdout.flush()
+  im_pantalla() 
+
+def load_animation(mensaje):  
+    # Cadena que se mostrará cuando se cargue la aplicación    
+    ls_len = len(mensaje)  
+    # Cadena para crear la línea giratoria
+    animation = ". . . . . ."
+    anicount = 0      
+    # utilizado para realizar un seguimiento de la duración de la animación
+    counttime = 0        
+    i = 0                  
+    while (counttime != 100):
+        
+        time.sleep(0.075)               
+        
+        # convertir la cadena a lista ya que la cadena es inmutable
+        load_str_list = list(mensaje)          
+        
+        # x->obteniendo el código ASCII
+        x = ord(load_str_list[i])
+          
+        # y->para almacenar código ASCII alterado
+        y = 0                             
+  
+        # si el caracter es "." o " ", manténgalo inalterado cambiar mayúsculas a minúsculas y viceversa
+        if x != 32 and x != 46:             
+            if x>90:
+                y = x-32
+            else:
+                y = x + 32
+            load_str_list[i]= chr(y)
+          
+        # para almacenar la cadena resultante
+        res =''             
+        for j in range(ls_len):
+            res = res + load_str_list[j]
+        
+        # mostrando la cadena resultante
+        sys.stdout.write("\r"+res + animation[anicount])
+        sys.stdout.flush()
+        mensaje = res
+  
+        anicount = (anicount + 1)% 4
+        i =(i + 1)% ls_len
+        counttime = counttime + 1
 
 def opcion_salir():
 
     im_pantalla()
-    cont = 0
-    while cont <=2:
-        time.sleep(0.6)
-        print('saliendo del programa.....')
-        im_pantalla()
-        if cont ==2:
-            time.sleep(0.8)
-            print("⣿⣿⣿⣿⣿⣿⣿⣿⡇GRACIAS⣿⣿⣿⣿⣿⣿⣿⣿⡇" + "\n" "⣿⣿⣿⣿⣿⣿VUELVA PRONTO⣿⣿⣿⣿⣿")
-        cont += 1
+    load_animation('saliendo del programa')
+
+    print( )
+    print("⣿⣿⣿⣿⣿⣿⣿⣿⡇GRACIAS⣿⣿⣿⣿⣿⣿⣿⣿⡇" + "\n" "⣿⣿⣿⣿⣿⣿VUELVA PRONTO⣿⣿⣿⣿⣿")
 
 def im_pantalla():
 
@@ -32,13 +84,14 @@ def anterior():
     gg = int(input('Ingrese una opcion:'))
     if gg == 1:
         im_pantalla()
+        animacion_de_carga()
     else:
         return False
 
 def menu():
 
   while True:
-    print("\n\n⣿⣿⣿⣿⣿⣿⣿⣿⡇VENTA DE AUTOS⣿⣿⣿⣿⣿⣿⣿⣿⡇")
+    print("\n⣿⣿⣿⣿⣿⣿⣿⣿⡇VENTA DE AUTOS⣿⣿⣿⣿⣿⣿⣿⣿⡇")
     print()
     
     print("1   Registrar Autos \n2   Autos disponibles\n3   Comprar Auto \n4   salir")
@@ -47,6 +100,7 @@ def menu():
 
     if opcion == 1:
         im_pantalla()
+        animacion_de_carga()
         opciones.registro()
         
         salir = anterior()
